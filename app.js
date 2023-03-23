48ec43a83eb845e312c80c9e54b728cdf4f625df
fredcoutinho52 <fredcoutinho52@gmail.com>
2023-03-23 17:49:12 -0300

    adicionando cards

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("carregou");
+function createCard() {
+  // capturar o elemento modelo
+  const elementModel = document.querySelector(".card");
+
+  // capturar o container dos cards
+  const container = document.querySelector(".cards-container");
+
+  // criar um clone do modelo
+  const card = elementModel.cloneNode(true);
+
+  // inserir o clone no container
+  container.insertAdjacentElement("beforeend", card);
+}
